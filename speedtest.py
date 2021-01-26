6c20c94ae3f98f782ed812e7c2d858f06d3bd717
Michael Madden <mikemadden42@icloud.com>
2021-01-25 19:15:45 -0600

    - Remove print statements. - Add upload speed to plot. - Add legend to chart.

--- speedtest.py
+++ speedtest.py
@@ -21,32 +21,17 @@ if __name__ == "__main__":
     list_versions()
 
     df = pd.read_csv("speedtest/speedtest.csv", parse_dates=["Timestamp"])
-
     df = df.drop(["Share"], axis=1)
-    print("=" * 16)
-    print(df)
-
-    print("=" * 16)
-    print(df.columns)
-
-    print("=" * 16)
-    print(df["Download"])
-
-    print("=" * 16)
-    print(df.head(4))
-
-    print("=" * 16)
-    print(df.tail(4))
-
-    print("=" * 16)
-    print(df.describe())
-
     sample_time = df["Timestamp"]
     download_speed = df["Download"]
+    upload_speed = df["Upload"]
+
     plt.figure(figsize=(16, 9))
     plt.title("Internet Speed")
     plt.xlabel("time")
     plt.gcf().axes[0].yaxis.get_major_formatter().set_scientific(False)
     plt.ylabel("speed")
-    plt.plot_date(sample_time, download_speed, linestyle="solid")
+    plt.plot_date(sample_time, download_speed, linestyle="solid", label="download")
+    plt.plot_date(sample_time, upload_speed, linestyle="solid", label="upload")
+    plt.legend()
     plt.show()
